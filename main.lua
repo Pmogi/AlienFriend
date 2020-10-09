ddbde4a6dadd22a3bd0702a472c202d63635ddb5
pmogiane <pmogiane@ucsc.edu>
2020-10-09 13:43:22 -0700

    Cleaned out main.lua

--- main.lua
+++ main.lua
@@ -10,40 +10,15 @@ local PlayGame = 1
 local PlayMenu = 0
 
 function love.load()
-    math.randomseed(os.time())
-    Menu.new()
-    gameState = PlayMenu
-    
-
 end
 
 function love.draw( )
-    if (gameState == PlayMenu) then
-        Menu.draw()
-    else
-        Game.draw()
-    end
 end
 
 function love.update( dt )
     Timer.update(dt)
-
-    if (gameState == PlayMenu) then
-        -- check for update from player on menu
-        Menu.update(dt)
-
-        if (Menu.returnMenuActivity() == false) then
-            gameState = PlayGame
-            startGame()
-        end
-
-    -- if it's not in the menu it's the game
-    else 
-        Game.update(dt) 
-    end
 end
 
 function startGame()
     Game.new()
-    gameState = PlayGame
 end
