c168d7b8b22a07aced1d5ec77aadf509b8d05c31
sqldatabunny <hitech.bunny.00@gmail.com>
2020-10-09 18:02:28 -0600

    I setup a test gameboard.

--- main.lua
+++ main.lua
@@ -5,18 +5,25 @@ local Timer = require("lib/timer")
 local Game = require("src/states/game")
 local Menu = require("src/states/menu")
 
+local Board = require("src/gameboard")
+
 local gameState
 local PlayGame = 1
 local PlayMenu = 0
 
+
+
 function love.load()
+    
 end
 
-function love.draw( )
+function love.draw()
+    Board:draw()
 end
 
-function love.update( dt )
+function love.update(dt)
     Timer.update(dt)
+    Board.update(dt)
 end
 
 function startGame()
