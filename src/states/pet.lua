10377b4a7fbffa727e8f7479844a94d29e7c0094
pmogiane <pmogiane@ucsc.edu>
2020-10-17 21:27:03 -0700

    Slime pet prototype is pretty much done.

--- src/states/pet.lua
+++ src/states/pet.lua
@@ -21,32 +21,35 @@ end
 
 function Pet.draw()
     love.graphics.draw(Assets.getAsset("ocean"), 20, -50, 0)
+    
+    --Draw the slime
+    slime:draw()
+    
 
+    love.graphics.setColor(0, 0, 0)
     love.graphics.rectangle("fill", 0, love.graphics.getHeight()-50, love.graphics.getWidth(), 50)
+    love.graphics.setColor(1, 1, 1)
+    
 
     local happy, hungry, growth = slime:returnStats()
     local heat, humidity, gravity = Environment.returnEnvStats()
 
-    love.graphics.print( "Happiness: " ..  happy .. 
-                         "\nHunger: " .. hungry ..
-                        '\nGrowth: ' .. growth)
-
+    local statString = string.format("Happiness: %2.2f, Hunger: %2.2f, Growth: %2.2f", happy, hungry, growth)
     local rescString = string.format( "Credits: %d Antimatter: %d AC Units: %d Slime Feed: %d ",  Resource.returnResource())    
-    local envString  = string.format( "%2.2f C, %2.2f Absolute Humidity, %2.2f N", heat*2.6, humidity/10, gravity/10)
-    love.graphics.print(rescString, 300)
-    love.graphics.print(envString, 300, 20)
-    
-    --Draw the slime
-    slime:draw()
+    local envString  = string.format( "%2.2f C, %2.2f%% Humidity, %2.2f N", heat*2.6, humidity/10, gravity/10)
+
+    love.graphics.print(statString, 300, love.graphics.getHeight()-45)
+    love.graphics.print(envString, 300, love.graphics.getHeight()-30)
+    love.graphics.print(rescString, 300, love.graphics.getHeight()-18)
     
     -- draws the gui
     suit.draw()
 end
 
 function GUI()
-    local am, ac, food = Resource.returnResource()
+    local credits, am, ac, food = Resource.returnResource()
     
-    if suit.Button("Feed", 100, love.graphics.getHeight()-50, 50, 50).hit then
+    if suit.Button("Feed", 0, love.graphics.getHeight()-50, 50, 50).hit then
         if (food > 0) then
             toastMessage("-1 Slime Food", 100, love.graphics.getHeight()-75, 300, 30)
             slime.feed()
@@ -57,64 +60,67 @@ function GUI()
 
     
     -- Heat Buttons
-    if suit.Button("Heat +", 150, love.graphics.getHeight()-50, 50, 25).hit then
+    if suit.Button("Heat +", 50, love.graphics.getHeight()-50, 50, 25).hit then
         if (ac > 0) then
             Environment.modifyHeat(10)
         else
-            toastMessage("No AC D:", 150, love.graphics.getHeight()-75, 300, 30)
+            toastMessage("No AC D:", 50, love.graphics.getHeight()-75, 300, 30)
         end
     end
 
-    if suit.Button("Heat -", 150, love.graphics.getHeight()-25, 50, 25).hit then
+    if suit.Button("Heat -", 50, love.graphics.getHeight()-25, 50, 25).hit then
         if (ac > 0) then
             Environment.modifyHeat(-10)
         else
-            toastMessage("No AC D:", 150, love.graphics.getHeight()-75, 300, 30)
+            toastMessage("No AC D:", 50, love.graphics.getHeight()-75, 300, 30)
         end
     end
     
     
     -- Humidity Buttons
-    if suit.Button("Humidty +", 200, love.graphics.getHeight()-50, 75, 25).hit then
+    if suit.Button("Humidty +", 100, love.graphics.getHeight()-50, 75, 25).hit then
         if (ac > 0) then
             Environment.modifyHumidity(10)
         else
-            toastMessage("No AC D:", 200, love.graphics.getHeight()-75, 300, 30)
+            toastMessage("No AC D:", 100, love.graphics.getHeight()-75, 300, 30)
         end
     end
 
-    if suit.Button("Humidity -", 200, love.graphics.getHeight()-25, 75, 25).hit then
+    if suit.Button("Humidity -", 100, love.graphics.getHeight()-25, 75, 25).hit then
         if (ac > 0) then
             Environment.modifyHumidity(-10)
         else
-            toastMessage("No AC D:", 200, love.graphics.getHeight()-75, 300, 30)
+            toastMessage("No AC D:", 100, love.graphics.getHeight()-75, 300, 30)
         end
     end
 
     
     -- Gravity Buttons
-    if suit.Button("Gravity +", 275, love.graphics.getHeight()-50, 75, 25).hit then
+    if suit.Button("Gravity +", 175, love.graphics.getHeight()-50, 75, 25).hit then
         if (ac > 0) then
             Environment.modifyGravity(10)
         else
-            toastMessage("No Antimatter D:", 275, love.graphics.getHeight()-75, 300, 30)
+            toastMessage("No Antimatter D:", 175, love.graphics.getHeight()-75, 300, 30)
         end
     end
 
-    if suit.Button("Gravity -", 275, love.graphics.getHeight()-25, 75, 25).hit then
+    if suit.Button("Gravity -", 175, love.graphics.getHeight()-25, 75, 25).hit then
         if (ac > 0) then
             Environment.modifyGravity(-10)
         else
-            toastMessage("No Antimatter D:", 275, love.graphics.getHeight()-75, 300, 30)
+            toastMessage("No Antimatter D:", 175, love.graphics.getHeight()-75, 300, 30)
         end
     end
+
+
 
 
 end
 
+-- Poops up a message for a second
 function toastMessage(msg, x, y, w, h)
     assert(type(msg) == "string", "Toast message recieved not a string")
-    Timer.during(2, function() suit.Label(msg, {color = colorToast}, x, y) end)
+    Timer.during(0.5, function() suit.Label(msg, {color = colorToast}, x, y) end)
 end
 
 function Pet.update(dt)
