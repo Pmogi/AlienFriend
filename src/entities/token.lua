58d0be4f0f2773dde8840831515c7361eaa0b4e2
sqldatabunny <hitech.bunny.00@gmail.com>
2020-10-21 20:54:13 -0600

    Added tokenmaker and updated token and game code to accommadate

--- src/entities/token.lua
+++ src/entities/token.lua
@@ -5,43 +5,62 @@ local Object = require("lib/classic")
 local Assets = require("src/assets")
 
 
-local Token = Object:extend()
+local token = Object:extend()
 
 
 
-function Token:new(x, y, type, world)
+function token:new(creator, index, x, y, type)
+    self.creator = creator
+    self.index = index
     self.x = x 
     self.y = y
-    if type == "AC" then self.img = Assets.getAsset("token1") end
-    if type == "antimatter" then self.img = Assets.getAsset("token2") end
-    if type == "food" then self.img = Assets.getAsset("token3") end
+    self.w = 50
+    self.h = 50
+    self.speed = 0
+    self.gravity = 0.01
+    self.gravity_max = 0
 
-    -- physics, body, shape, fixture
-    self.body = love.physics.newBody(world, self.x, self.y, "static")
-    self.shape = love.physics.newCircleShape(15) -- radius of 10 pixels
-    self.fixture = love.physics.newFixture(self.body, self.shape, self.density) -- 
-
-    self.fixture:setSensor(true)
-    
-    self.depth = 1
-    self.colors = {1, 1, 1}
-
-    -- self.resource = self:assignResource()
-    self.resourceAmount = 1
-    
-    self.id = "Token"
-
-    self.fixture:setUserData({id = self.id, alive = true})
+    if type == "AC" then
+        self.gravity_max = 0.1
+        self.img = Assets.getAsset("token1") end
+    if type == "Antimatter" then
+        self.gravity_max = 0.1
+        self.img = Assets.getAsset("token2") end
+    if type == "Food" then
+        self.gravity_max = 0.1
+        self.img = Assets.getAsset("token3") end
+    return self
 end
 
-function Token:draw()
-    love.graphics.draw(self.img, self.body:getX(), self.body:getY())
+function token:draw()
+    --print(self.x .. " | " .. self.y)
     love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(self.img, self.x, self.y)
+    
 end
 
-function Token:update(dt)
-    self.y = self.y + 1
+function token:update(dt)
+    --print(self.y .. "  |  " .. self.creator:getDestructionPoint())
+    if self.y > self.creator:getDestructionPoint() then
+        --print("boundary destruction")
+        self.creator:destroy(self.index)
+        return
+    end
+
+    local xPos =  love.mouse.getX()
+    local yPos =  love.mouse.getY()
+    if ( xPos > self.x and xPos < self.x+self.w and
+            yPos > self.y and yPos < self.y + self.h ) then
+        if love.mouse.isDown(1) then
+            --print("mouse destruction")
+            self.creator:destroy(self.index)
+            return
+        end
+    end
+
+    self.speed = math.min(self.speed + self.gravity, self.gravity_max)
+    self.y = self.y + self.speed
+    
 end
 
-
-return Token
+return token
--- src/states/game.lua
+++ src/states/game.lua
@@ -4,24 +4,28 @@ local Timer = require("lib/timer")
 local GameBoard = require("src/gameboard")
 local Boardmaker = require("src/boardmaker")
 local Resource = require("src/systems/resource")
-
+local TokenMaker = require("src/entities/tokenmaker")
 
 local Game = {}
 -- Modules
 local gameBoard
+local tokenMaker
 
 function Game.new()
     Resource.init()
-    gameBoard = GameBoard()
+    tokenMaker = TokenMaker()
+    --gameBoard = GameBoard()
     -- boardMaker = Boardmaker(720,720,gameBoard)
 end
 
 function Game.draw()
-    gameBoard.draw()
+    tokenMaker:draw()
+    --gameBoard.draw()
 end
 
 function Game.update(dt)
-    gameBoard.update(dt)
+    tokenMaker:update(dt)
+    --gameBoard.update(dt)
     --boardMaker.update(dt)
 end
 
